463525b18c616995dbae76898f5ffb34cf67da75
Jean Estadieu <jean.estadieu@etud.univ-jfc.fr>
2024-10-14 20:46:11 +0200

    JE:details_series_creat

--- app/src/main/java/com/example/my_application1/ui/SeriesSelected.kt
+++ app/src/main/java/com/example/my_application1/ui/SeriesSelected.kt
@@ -28,114 +28,219 @@ fun SeriesSelected(navController: NavController, viewModel: MainViewModel, id: S
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = seriesSelected.value.name,
-                fontWeight = FontWeight.Bold,
-                fontSize = 30.sp,
-                fontFamily = FontFamily.Default
-            )
-
-            AsyncImage(
-                model = "https://image.tmdb.org/t/p/w500/" + seriesSelected.value.backdrop_path,
-                contentDescription = null,
-            )
-
-            Text(
-                text = seriesSelected.value.first_air_date,
-                fontWeight = FontWeight.Light,
-                fontSize = 20.sp,
-                fontFamily = FontFamily.Serif
-            )
-
-            Text(
-                text = "Language: " + seriesSelected.value.original_language,
-                fontWeight = FontWeight.Normal,
-                fontSize = 20.sp,
-                fontFamily = FontFamily.Default
-            )
-
-            Text(
-                text = "Popularity: " + seriesSelected.value.popularity,
-                fontWeight = FontWeight.Normal,
-                fontSize = 20.sp,
-                fontFamily = FontFamily.Default
-            )
-
-            Text(
-                text = "Vote: " + seriesSelected.value.vote_count,
-                fontWeight = FontWeight.Normal,
-                fontSize = 20.sp,
-                fontFamily = FontFamily.Default
-            )
-            Spacer(modifier = Modifier.padding(10.dp))
-
-            Text(
-                text = "Synopsis: " + seriesSelected.value.overview,
-                fontWeight = FontWeight.Normal,
-                fontSize = 20.sp,
-                fontFamily = FontFamily.Default,
-                textAlign = TextAlign.Justify
-            )
-        }
-    } else {
-        Row(
-            Modifier
-                .fillMaxSize()
-                .padding(start = 20.dp), verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column(verticalArrangement = Arrangement.SpaceEvenly) {
+            // Vérification si le nom n'est pas null
+            seriesSelected.value.name?.let { name ->
                 Text(
-                    text = seriesSelected.value.name,
+                    text = name,
                     fontWeight = FontWeight.Bold,
                     fontSize = 30.sp,
                     fontFamily = FontFamily.Default
                 )
-                Spacer(modifier = Modifier.padding(18.dp))
+            }
 
+            // Vérification si le chemin d'image du backdrop n'est pas null
+            seriesSelected.value.backdrop_path?.let { backdropPath ->
                 AsyncImage(
-                    model = "https://image.tmdb.org/t/p/w500/" + seriesSelected.value.backdrop_path,
+                    model = "https://image.tmdb.org/t/p/w500/$backdropPath",
                     contentDescription = null,
                 )
+            }
 
+            // Vérification si la date de première diffusion n'est pas null
+            seriesSelected.value.first_air_date?.let { firstAirDate ->
                 Text(
-                    text = seriesSelected.value.first_air_date,
-                    fontWeight = FontWeight.Normal,
+                    text = firstAirDate,
+                    fontWeight = FontWeight.Light,
                     fontSize = 20.sp,
-                    fontFamily = FontFamily.Default
+                    fontFamily = FontFamily.Serif
                 )
+            }
 
+            // Vérification si la langue originale n'est pas null
+            seriesSelected.value.original_language?.let { originalLanguage ->
                 Text(
-                    text = "Language: " + seriesSelected.value.original_language,
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 20.sp,
-                    fontFamily = FontFamily.Default
-                )
-
-                Text(
-                    text = "Popularity: " + seriesSelected.value.popularity,
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 20.sp,
-                    fontFamily = FontFamily.Default
-                )
-
-                Text(
-                    text = "Vote: " + seriesSelected.value.vote_count,
+                    text = "Language: $originalLanguage",
                     fontWeight = FontWeight.Normal,
                     fontSize = 20.sp,
                     fontFamily = FontFamily.Default
                 )
             }
 
-            Spacer(modifier = Modifier.padding(26.dp))
+            // Vérification si la popularité n'est pas null
+            Text(
+                text = "Popularity: ${seriesSelected.value.popularity}",
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Default
+            )
 
-            Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.SpaceEvenly) {
+            // Vérification si le nombre de votes n'est pas null
+            Text(
+                text = "Vote: ${seriesSelected.value.vote_count}",
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Default
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            // Vérification si le synopsis (overview) n'est pas null
+            seriesSelected.value.overview?.let { overview ->
                 Text(
-                    text = "Synopsis: " + seriesSelected.value.overview,
+                    text = "Synopsis: $overview",
                     fontWeight = FontWeight.Normal,
                     fontSize = 20.sp,
                     fontFamily = FontFamily.Default,
                     textAlign = TextAlign.Justify
                 )
+            }
+
+            // Vérification si la liste des créateurs n'est pas vide
+            if (seriesSelected.value.created_by.isNotEmpty()) {
+                Text(
+                    text = "Created by: ",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.Default,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+
+                seriesSelected.value.created_by.forEach { creator ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        // Vérification si le profil du créateur est renseigné
+                        creator.profile_path?.let { profilePath ->
+                            AsyncImage(
+                                model = "https://image.tmdb.org/t/p/w200/$profilePath",
+                                contentDescription = creator.name,
+                                modifier = Modifier.size(50.dp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        // Affichage du nom du créateur s'il n'est pas null
+                        Text(
+                            text = creator.name ?: "Unknown",
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 18.sp,
+                            fontFamily = FontFamily.Default
+                        )
+                    }
+                }
+            }
+        }
+    } else {
+        // Version pour les grands écrans (large)
+        Row(
+            Modifier
+                .fillMaxSize()
+                .padding(start = 20.dp), verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(verticalArrangement = Arrangement.SpaceEvenly) {
+                // Vérification si le nom n'est pas null
+                seriesSelected.value.name?.let { name ->
+                    Text(
+                        text = name,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Default
+                    )
+                }
+
+                Spacer(modifier = Modifier.padding(18.dp))
+
+                // Vérification si le chemin d'image du backdrop n'est pas null
+                seriesSelected.value.backdrop_path?.let { backdropPath ->
+                    AsyncImage(
+                        model = "https://image.tmdb.org/t/p/w500/$backdropPath",
+                        contentDescription = null,
+                    )
+                }
+
+                // Vérification si la date de première diffusion n'est pas null
+                seriesSelected.value.first_air_date?.let { firstAirDate ->
+                    Text(
+                        text = firstAirDate,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Default
+                    )
+                }
+
+                // Vérification si la langue originale n'est pas null
+                seriesSelected.value.original_language?.let { originalLanguage ->
+                    Text(
+                        text = "Language: $originalLanguage",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Default
+                    )
+                }
+
+                Text(
+                    text = "Popularity: ${seriesSelected.value.popularity}",
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.Default
+                )
+
+                Text(
+                    text = "Vote: ${seriesSelected.value.vote_count}",
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.Default
+                )
+
+                // Vérification si la liste des créateurs n'est pas vide
+                if (seriesSelected.value.created_by.isNotEmpty()) {
+                    Text(
+                        text = "Created by: ",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Default,
+                        modifier = Modifier.padding(top = 16.dp)
+                    )
+
+                    seriesSelected.value.created_by.forEach { creator ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.padding(8.dp)
+                        ) {
+                            creator.profile_path?.let { profilePath ->
+                                AsyncImage(
+                                    model = "https://image.tmdb.org/t/p/w200/$profilePath",
+                                    contentDescription = creator.name,
+                                    modifier = Modifier.size(50.dp)
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.width(8.dp))
+
+                            Text(
+                                text = creator.name ?: "Unknown",
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 18.sp,
+                                fontFamily = FontFamily.Default
+                            )
+                        }
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.padding(26.dp))
+
+            Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.SpaceEvenly) {
+                seriesSelected.value.overview?.let { overview ->
+                    Text(
+                        text = "Synopsis: $overview",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Default,
+                        textAlign = TextAlign.Justify
+                    )
+                }
             }
         }
     }
